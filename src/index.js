f59fac8742776f2e8e6b7792fdf62bc0f9593a2c
Simon Warta <simon@kullo.net>
2018-03-07 08:21:11 +0100

    Add rejection handlers to libsodium.ready

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import * as libsodium from "libsodium-wrappers";
 
 console.log("[Main thread] waiting for libsodium");
 
-libsodium.ready.then(function(sodium) {
+libsodium.ready
+  .then(function(sodium) {
     console.log("[Main thread] libsodium ready");
-});
+  })
+  .catch(function(error) {
+    console.error("[Main thread] Initializing libsodium errored:", error);
+  });
--- worker/cryptoworker.js
+++ worker/cryptoworker.js
@@ -2,6 +2,10 @@ import * as libsodium from "libsodium-wrappers";
 
 console.log("[Worker] waiting for libsodium");
 
-libsodium.ready.then(function(sodium) {
+libsodium.ready
+  .then(function(sodium) {
     console.log("[Worker] libsodium ready");
-});
+  })
+  .catch(function(error) {
+    console.error("[Worker] Initializing libsodium errored:", error);
+  });
